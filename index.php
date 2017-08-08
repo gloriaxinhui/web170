ccc9b889c02e257278d99bb670f3f23b524ce763
gloriaxinhui <gloriaxinhui@gmail.com>
2017-08-08 01:16:30 -0700

    Custom Posts Feed with Featured Images
    Creating a Custom Posts Feed with Featured Images for Your Site

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php get_header(); ?>
 
-    <div class="blueberry">
-      <ul class="slides">
-      	<li><a href="creative.html"><img src="<?php bloginfo('template_directory'); ?>/images/slider/chicks.jpg" alt="chicks" title="Artworks by Gloria"></a></li>
-        <li><a href="fashion.html"><img src="<?php bloginfo('template_directory'); ?>/images/slider/fashion.jpg" alt="fashion" title="Fashion Design by Gloria"></a></li>
-        <li><a href="more.html"><img src="<?php bloginfo('template_directory'); ?>/images/slider/fun.jpg" alt="fun" title="More projects by Gloria"></a></li>
-        <li><a href="web.html"><img src="<?php bloginfo('template_directory'); ?>/images/slider/web.jpg" alt="web" title="Web Design by Gloria"></a></li>
-      </ul>
-  	</div> <!--end of blueberry-->
-   
-    <?php 
-        if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
-    <h1><a href="<?php the_permalink(); // link to the page or posting ?>"><?php the_title(); // get the page or posting title ?></a></h1>
-    <?php the_content(''); // get page or posting written content ?>
+   <!-- Begin Content -->
+    <div id="content">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
+    <article class="post-excerpt">
+    <h2><a href="<?php the_permalink(); // link to the page or posting ?>"><?php the_title(); // get the page or posting title ?></a></h2>
+    
+        <small>Posted on <?php the_time('F j, Y'); //get the time ?> by <?php the_author(); //get the author ?> in <?the_category(', '); // get the category ?></small>
+    
+    <a href="<?php the_permalink(); // link to the page or posting ?>">
+    <?php the_post_thumbnail( 'thumbnail' ); ?></a>
+    <?php the_excerpt(); // get the posting's excerpt ?>
+        <p class="read-more"><a href="<?php the_permalink(); //link to the page or posting ?>">Read More</a></p>
+    </article>
     <?php endwhile; endif; // end the loop ?>
+    
     <small>index.php</small>
-        
+    </div> <!-- End Content -->
+<?php get_sidebar() ?>
 <?php get_footer() ?>
     
--- page.php
+++ page.php
@@ -4,11 +4,16 @@
     <div id="content">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
     
+    <article id="post-<?php the_ID(); ?>" class="post">
+    <h2><?php the_title(); // get page or posting title ?></h2>
+    <small>Posted on <?php the_time('F j, Y'); //get the time ?> by <?php the_author(); // get the author name ?> in <?php the_category(', '); // get the category ?></small>
+    <?php the_post_thumbnail( 'large' ); //get the featured image ?>
     <?php the_content(''); // get page or posting written content ?>
     <?php endwhile; endif; // end the loop ?>
+        
     <small>page.php</small>
-    </div>
-    <!-- End Content -->
+    </article>
+    </div> <!-- End Content -->
     
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
